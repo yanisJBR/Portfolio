b4cb40abfe1e5cfb4ced862c04630d21a592c6e2
YanisJBR <yanis.jabri.10@gmail.com>
2023-10-20 16:37:37 +0200

    oueoueoue

--- exo.sql
+++ exo.sql
@@ -3,7 +3,10 @@ CREATE DATABASE Basededonnee;
 USE Basededonnee;
 
 CREATE TABLE A(
-    id int primary key
-    nom varchar(20)
-    
-)
+    id int primary key,
+    nom varchar(20),
+    modelvehicule varchar(20),
+    numeroplaque varchar(9)
+    prix int,
+    kilometrage int,
+);
